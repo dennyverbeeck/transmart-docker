9df8824eaf70647532558cff0a0ea9fc6b7a1894
dennyverbeeck <dennyverbeeck@gmail.com>
2016-09-21 17:15:02 +0200

    * Check for actual number in TOTALMEM rather than just non-emptyness check  * 10 max connections is too low, increasing to 20

--- transmart-db/tunepgsql.sh
+++ transmart-db/tunepgsql.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 TOTALMEM=$(grep MemTotal /proc/meminfo | awk '{print $2;}')
-if [ -z $TOTALMEM ]; then
-  echo "tunepgsql.sh: Could not auto-detect total memory, setting to default of 8GB"
-  TOTALMEM=$((8*1024))
+if ! [[ "$TOTALMEM" =~ ^[0-9]+$ ]]; then
+  echo "tunepgsql.sh: Could not auto-detect total memory: $TOTALMEM, setting to default of 8GB"
+  TOTALMEM=$((8*1024*1024))
 else
   echo "tunepgsql.sh: Total memory auto-detected: $TOTALMEM kB"
 fi
 
 cat >> /etc/postgresql/9.3/main/postgresql.conf <<EOD
-max_connections = 10
+max_connections = 20
 shared_buffers = $((TOTALMEM/4))kB
 effective_cache_size = $((TOTALMEM*3/4))kB
-work_mem = $((TOTALMEM/4/60))kB  # (Totalmem - shared_buffers) / (max_connections * 3) / 2
+work_mem = $((TOTALMEM/4/120))kB  # (Totalmem - shared_buffers) / (max_connections * 3) / 2
 maintenance_work_mem = $((TOTALMEM/8))kB
 checkpoint_segments = 128
 checkpoint_completion_target = 0.9
