a78eb2f5d1233e571f0dc5eab0465ca895d098c1
Denny Verbeeck <dennyverbeeck@gmail.com>
2016-10-06 15:07:06 +0000

    Added a line that fixes a Go issue where on some systems, the lookup order is dns,file; thus preventing to resolve localhost

--- transmart-web/start.sh
+++ transmart-web/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-/usr/bin/filebeat -e -c /etc/filebeat/filebeat.yml &
+# if you get error messages regarding unable to resolve the name 'localhost', uncomment
+# this to switch the go resolver, as the order might be dns server first, file second
+# export GODEBUG=netdns=cgo
+/usr/bin/filebeat -e -v -c /etc/filebeat/filebeat.yml &
 httpd-foreground
