901c3108450343fb8111cd0384ea031562a5a21d
Denny Verbeeck <dennyverbeeck@gmail.com>
2016-10-12 15:32:58 +0000

    Removed verbose logging for filebeat in tmweb. Added restart policy to containers so they start on system start.

--- transmart-web/start.sh
+++ transmart-web/start.sh
@@ -3,5 +3,5 @@
 # if you get error messages regarding unable to resolve the name 'localhost', uncomment
 # this to switch the go resolver, as the order might be dns server first, file second
 # export GODEBUG=netdns=cgo
-/usr/bin/filebeat -e -v -c /etc/filebeat/filebeat.yml &
+/usr/bin/filebeat -e -c /etc/filebeat/filebeat.yml &
 httpd-foreground
